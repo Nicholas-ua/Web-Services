999ede1d29bb94b5bf285c7a8eb6bb2f846afcd3
Nicholas-ua <m.savych@gmail.com>
2017-12-14 15:49:41 +0200

    Initial Commit

---
+++ PlantPlacesWebService/PlantPlacesWebService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlantPlacesWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
