2e6016c592f65589bb523979bd6ece02a5996f49
Nicholas-ua <m.savych@gmail.com>
2017-12-14 15:56:05 +0200

    Insert ReadKey()

--- PlantPlacesWebService/PlantPlacesWebService/Program.cs
+++ PlantPlacesWebService/PlantPlacesWebService/Program.cs
@@ -7,6 +7,7 @@ namespace PlantPlacesWebService
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
